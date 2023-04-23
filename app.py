1d4e2ad2548cba19fa196bf6853ba41f86355f08
Abed-alrahman1 <93323471+Abed-alrahman1@users.noreply.github.com>
2023-04-23 13:54:02 +0300

    BSPMC2329-27
    BSPMC2329-27

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ cursor = db.cursor
 #init app
 app = Flask("app")
 
+@app.route('/login',methods=['POST'])
+def login():
+    temp = User()
+    temp.email, temp.password = request.form['email'],request.form['password']
+    if db.login(temp):
+        user = db.get_user_by_email(temp.email).totuple()
+        return jsonify({'message': 'Login successful','user': user})
+    else:
+        return jsonify({'message': 'Invalid username or password'})
+
 if __name__ == '__main__':
     app.run(debug=True)
