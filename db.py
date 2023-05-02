e255fdead233a96711e4917e26ede2284d5d9e4a
Eden <rwashdi.eden@gmail.com>
2023-05-02 15:32:00 +0300

    BSPMC2329-6
    BSPMC2329-6

--- db.py
+++ db.py
@@ -32,3 +32,28 @@ class UserDb:
     # Create a table named Users
     def create_user_table(self):
         self.cursor.execute("CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY , email VARCHAR(255) , password VARCHAR(255) , type int(2) , name VARCHAR(255) , lastname VARCHAR(255))")
+    def insert_user(self,user):
+        query = "INSERT INTO users (email, password,type,name,lastname) VALUES (%s, %s,%s, %s,%s)"
+
+        #check if valid email
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        if not re.search(regex, user.email):
+            lg.debug('not a valid email')
+            lg.debug("not great success")
+            return False
+
+        #check if there is username with this email
+        self.cursor.execute("SELECT email FROM users")
+        result = self.cursor.fetchall()
+        for i in result:
+            if i[0] == user.email:
+                lg.debug(f'there is {user.email} in db')
+                lg.debug("not great success")
+                return False
+
+        #insert
+        self.cursor.execute(query,user.totuple())
+        self.mydb.commit()
+        lg.debug(self.cursor.rowcount, "record(s) inserted.")
+        lg.debug("great success")
+        return True
